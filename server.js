073fdf36c00687787a6a2e937551d7a2160d00e3
Joao C Barbosa <joaao@outlook.com>
2020-09-15 17:40:23 -0300

    feat: set up broadcast when a new user enter or leave the room

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 require('dotenv/config');
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
-const path = require('path');
+
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 3000, () => {
+// Run when a client connects
+io.on('connection', (socket) => {
+    console.log('New websocket connection');
+
+    // Send a message only to the user connected
+    socket.emit('message', 'Welcome to ChatCord');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when clinet disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+});
+
+server.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 });
