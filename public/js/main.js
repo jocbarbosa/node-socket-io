4a77521be396acee3f378c4f8b6a04f14903d1e6
Joao C Barbosa <joaao@outlook.com>
2020-09-21 20:22:19 -0300

    feat: scroll down messages when text reachs the box height

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 const chatForm = document.querySelector('#chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 socket.on('message', message => {
   console.log(message);
   outputMessage(message);
+
+  // Scroll the page down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Submit message
@@ -15,16 +19,21 @@ chatForm.addEventListener('submit', (e) => {
 
   // Emit message to server
   socket.emit('chatMessage', msg);
+
+  outputMessage(msg);
 });
 
 // Output message to DOM
 const outputMessage = (message) => {
   const div = document.createElement('div');
   div.classList.add('message');
-  div.innerHTML = `<p class="meta">Brad <span>9:00 pm</span></p>
+  div.innerHTML = `<p class="meta">Brad <span>9:00</span></p>
     <p class="text">
       ${message}
   </p>`;
 
   document.querySelector('.chat-messages').appendChild(div);
+
+  const msgField = document.querySelector('#msg');
+  msgField.value = '';
 }
